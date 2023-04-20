59cca836dbadb98bcf9997762315fbb60b692a1f
IsabelleAnn <isabelle.ann85@gmail.com>
2023-04-20 17:20:11 -0500

    Replace dice numbers with dots

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,14 @@
+import Dot from "./Dot.jsx"
 export default function Die(props){
+        let dotElements = [];
+        for(let i=1; i<=props.value; i++){
+            dotElements.push(<Dot value={props.value}/>)
+        }
+
     return(
     <div 
         onClick={()=>props.handleDieClick(props.id)} 
         style={props.isLocked ? {backgroundColor: "#59E391"} : {}} 
-        className="die">{props.value}
+        className="die">{dotElements}
     </div>)
 }
