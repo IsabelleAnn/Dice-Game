31d0fb99b44a1df305890ece50b08e5c57c5727d
IsabelleAnn <isabelle.ann85@gmail.com>
2023-04-20 18:01:17 -0500

    Style dot positioning

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -5,10 +5,32 @@ export default function Die(props){
             dotElements.push(<Dot value={props.value}/>)
         }
 
+        let diceStyle = "";
+        switch(props.value){
+            case 1:
+                diceStyle = "first-face";
+                break;
+            case 2:
+                diceStyle = "second-face";
+                break;
+            case 3:
+                diceStyle = "third-face";
+                break;
+            case 4:
+                diceStyle = "fourth-face";
+                break;
+            case 5:
+                diceStyle = "fifth-face";
+                break;
+            case 6:
+                diceStyle = "sixth-face";
+                break;
+        }
+
     return(
     <div 
         onClick={()=>props.handleDieClick(props.id)} 
-        style={props.isLocked ? {backgroundColor: "#59E391"} : {}} 
-        className="die">{dotElements}
+        style={props.isLocked ? {backgroundColor: "#0b2434"} : {}} 
+        className={`die ${diceStyle}`}>{dotElements}
     </div>)
 }
