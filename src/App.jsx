2d131f39c3de674e16dda77fb984965ee7b942f1
IsabelleAnn <isabelle.ann85@gmail.com>
2023-04-26 17:43:47 -0500

    Create game timer, best time and roll count

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import { useState } from 'react'
-import Die from './components/Die.jsx'
-import Header from './components/Header.jsx';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { useEffect } from 'react';
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
+import Die from './components/Die.jsx';
+import Header from './components/Header.jsx';
+import Stopwatch from './components/Stopwatch.jsx';
+import Rolls from './components/Rolls.jsx';
 
 export default function App() {
-
+  let [timerInterval, setTimerInterval] = useState(null);
   let [dice, setDice] = useState(allNewDice(10));
   let [tenzies, setTenzies] = useState(false);
+  let [rolls, setRolls] = useState(0);
+  let [times, setTimes] = useState({
+    hour:0,
+    min: 0,
+    sec: 0,
+    best: {hour:0,min:0,sec:0}
+  });
+  //---------------------------------------------
 
-  let diceElements = dice.map(die=>{
-    return( <Die 
-      handleDieClick={handleDieClick} 
-      isLocked={die.isLocked} 
-      value={die.value}
-      key={die.id}
-      id={die.id}
-      />)
-  })
+  function stopTimer(){
+    clearInterval(timerInterval);
+    setTimerInterval(null);
+    console.log("Timer stopped")
+  }
+
+  function startTimer(){
+    setTimerInterval(setInterval(timer, 1000));
+  }
+
+  function timer(){
+    setTimes(prevTimes=>{
+      let newSec = prevTimes.sec;
+      let newMin = prevTimes.min;
+      let newHour = prevTimes.hour;
+     
+    newSec++;
+    if(newSec >= 60){
+      newSec =0;
+      newMin++;
+    }
+    if(newMin >= 60){
+      newMin=0;
+      newHour++;
+    }
+    return {...prevTimes, sec: newSec, min: newMin, hour: newHour}
+    })
+    
+
+  }
  
+//-------------------------------------------------
+
+
   function allNewDice(length){
     const numberOfDies = length;
     let numberArray = [];
@@ -42,24 +76,24 @@ export default function App() {
 
   function handleBtnClick(){
     if(tenzies){
-      setDice(allNewDice(10))
+      setDice(allNewDice(10));
+      setTimes(prevTimes=>{
+        return {...prevTimes, hour: 0, min: 0, sec:0}
+      })
+      setRolls(0);
+      stopTimer();
+      
     }else{
+      setRolls((prevRolls)=>prevRolls+1);
       setDice(prevDice => prevDice.map(die=>{
         return !die.isLocked ? {...die, value: getRandomDiceValue()} : die
-    }))
+      }));
+      if(rolls === 0){
+        startTimer();
+        console.log("Timer set")
+      }
     }
   }
-  
-  useEffect(()=>{
-    setTenzies(dice.every((die)=>die.value === dice[0].value))
-  }, [dice]);
-
-  useEffect(()=>{
-    if(tenzies){
-      setDice(prevDice => prevDice.map(die=> {
-        return die.isLocked===false ? {...die, isLocked: true} : die}))
-    }
-  }, [tenzies]);
 
   function handleDieClick(id){
     setDice(prevDice=>{ 
@@ -69,14 +103,73 @@ export default function App() {
     })
   }
 
+  useEffect(()=>{
+    setTenzies(dice.every((die)=>die.value === dice[0].value))
+  }, [dice]);
+
+  useEffect(()=>{
+    if(tenzies){
+      stopTimer();
+      setDice(prevDice => prevDice.map(die=> {
+        return die.isLocked===false ? {...die, isLocked: true} : die}))
+        console.log("setting best time");
+      setTimes(prevTimes=>{
+        if(prevTimes.best.hour == 0 &&
+          prevTimes.best.min == 0 &&
+          prevTimes.best.sec ==0){
+return {...prevTimes, 
+            best: {
+              hour: prevTimes.hour, 
+              min: prevTimes.min, 
+              sec: prevTimes.sec
+            }
+          }
+        }
+        if((prevTimes.hour < prevTimes.best.hour) ||
+          (prevTimes.hour == prevTimes.best.hour &&
+            prevTimes.min < prevTimes.best.min)||
+            (prevTimes.min == prevTimes.best.min &&
+            prevTimes.sec < prevTimes.best.sec)){
+                     return {...prevTimes, 
+            best: {
+              hour: prevTimes.hour, 
+              min: prevTimes.min, 
+              sec: prevTimes.sec
+            }
+          }
+        }
+        return prevTimes;
+      })
+      console.log("Best: ", times.best.hour, times.best.min, times.best.sec)
+    }
+  }, [tenzies]);
+
+    let diceElements = dice.map(die=>{
+    return( <Die 
+      handleDieClick={handleDieClick} 
+      isLocked={die.isLocked} 
+      value={die.value}
+      key={die.id}
+      id={die.id}
+      />)
+  })
+
   return (
+    <div className='content'>
     <main>
       {tenzies && <p className='win'>Woo-hoo!</p>}
       {tenzies && <Confetti width={window.innerWidth} height={window.innerHeight}/>}
       <Header/>
       <div className='dies-container'>{diceElements}</div>
       <button onClick={handleBtnClick} className='roll-btn'>{tenzies ? "Restart" : "Roll"}</button>
+      <div className='game-stats'>
+       <Rolls rolls={rolls}/>
+       <Stopwatch times={times}/>
+      </div>
     </main>
+      
+    </div>
+
   )
 }
 
